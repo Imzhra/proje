0b91bf3097145cf6d4f73adee2444d786963551a
Imzhra <zahra1dehqani@gmail.com>
2024-08-20 13:38:43 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <Firstform />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+
+import { defineComponent } from 'vue'
+
+
+import Firstform from './components/Firstform.vue';
+
+
+
+</script>
